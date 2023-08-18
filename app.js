fec36fb4bb80643eb369c22d2c7624527733df8b
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-08-18 22:47:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const endDate = "31 December 2023 11:59 PM";
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+function clock() {
+    const end = new Date(endDate);
+    const now = new Date();
+    // console.log(end, now);
+    const diff = (end - now) / 1000;
+    inputs[0].value = Math.floor(diff / 3600/24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+
+    
+
+}
+clock();
+
+setInterval(
+    () => {
+        clock();
+    },
+    1000  
+)
